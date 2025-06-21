9b90c2a2a0e23362da1020200a3b4d4cb820605c
Thiago Willecke <thiagogwillecke@gmail.com>
2025-06-20 23:14:51 -0300

    feat: add isTyping and send and receive messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,87 +1,175 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import useWebSocket from "react-use-websocket";
 
 function App() {
-  const [messages, setMessages] = useState<any[]>([])
-  const [textInput, setTextInput] = useState('')
-  const [isTyping, setIsTyping] = useState(false)
+	const WS_URL = "ws://localhost:8080";
+	const { sendJsonMessage, sendMessage, lastJsonMessage, readyState } =
+		useWebSocket(WS_URL, {
+			share: false,
+			shouldReconnect: () => true,
+		});
 
-  useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080')
-    ws.onmessage = (event) => {
-      const data = JSON.parse(event.data)
-      if (data.type === 'all_messages') {
-        setMessages(data.payload.messages)
-      }
-    }
-    ws.onopen = () => {
-      console.log('Connected to server')
-    }
-    ws.onclose = () => {
-      console.log('Disconnected from server')
-    }
-    ws.onerror = (error) => {
-      console.error('WebSocket error:', error)
-    }
-  }, [])
+	const [messages, setMessages] = useState<any[]>([]);
+	const [textInput, setTextInput] = useState("");
+	const [isTyping, setIsTyping] = useState(false);
+	const [isExternalTyping, setIsExternalTyping] = useState(false);
+	const [username, setUsername] = useState("");
+	const [usernameInput, setUsernameInput] = useState("");
 
-  useEffect(() => {
-    if (isTyping) console.log('isTyping')
-    else console.log('stopped typing')
-  }, [isTyping])
+	useEffect(() => {
+		if (lastJsonMessage) {
+			const message = lastJsonMessage as any;
+			if (message.type === "all_messages") {
+				setMessages(message.payload.messages);
+			} else if (message.type === "text") {
+				setMessages((prevMessages) => [...prevMessages, message]);
+			} else if (message.type === "typing_status") {
+				console.log(
+					"Received typing status:",
+					message.payload.isTyping,
+				);
+				if (message.payload.username === username) return;
+				setIsExternalTyping(message.payload.isTyping);
+			}
+		}
+	}, [lastJsonMessage]);
 
-  const handleSendMessage = () => {
-    if (textInput.length === 0 || isTextOnlyWhitespace(textInput)) return
-    // setMessages([...messages, { id: messages.length + 1, payload: { body: textInput, username: 'John Doe', timestamp: new Date().toLocaleTimeString().split(':')[0] + ':' + new Date().toLocaleTimeString().split(':')[1] } }])
-    setTextInput('')
-  }
+	useEffect(() => {
+		if (isTyping) {
+			console.log("isTyping");
+			sendJsonMessage({
+				type: "typing_status",
+				payload: {
+					isTyping: true,
+					username: username,
+				},
+			});
+			return;
+		}
+		if (!isTyping) {
+			console.log("stopped typing");
+			sendJsonMessage({
+				type: "typing_status",
+				payload: {
+					isTyping: false,
+					username: username,
+				},
+			});
+		}
+	}, [isTyping]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTextInput(e.target.value)
-    if (e.target.value.length > 0) setIsTyping(true)
-    else setIsTyping(false)
-  }
+	const handleSendMessage = () => {
+		if (textInput.length === 0 || isTextOnlyWhitespace(textInput)) return;
+		sendJsonMessage({
+			type: "text",
+			payload: {
+				body: textInput,
+				username: username,
+			},
+		});
+		setTextInput("");
+		setIsTyping(false);
+	};
 
-  const isTextOnlyWhitespace = (text: string) => text.trim() === ''
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTextInput(e.target.value);
+		if (e.target.value.length > 0) setIsTyping(true);
+		else setIsTyping(false);
+	};
 
-  const handleButtonDisabled = () => textInput.length === 0 || isTextOnlyWhitespace(textInput)
+	const isTextOnlyWhitespace = (text: string) => text.trim() === "";
 
-  return (
-    <>
-      <h1>Public Chat</h1>
-      {/* <div>
-        <input type="text" placeholder="Enter your username" />
-        <button>Enter</button>
-      </div> */}
-      <div className="chat-container">
-        <div className="message-frame">
-          <div>
-            {messages.map((message) => (
-              <div key={message.payload.id}>
-                <p>
-                  <span>{new Date(message.payload.timestamp).toLocaleTimeString()} ~ </span><b>{message.payload.username}</b>: {message.payload.body}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Enter your message"
-            value={textInput}
-            onChange={handleInputChange}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleSendMessage()
-              }
-            }}
-          />
-          <button onClick={handleSendMessage} disabled={handleButtonDisabled()}>Send</button>
-        </div>
-      </div>
-    </>
-  )
+	const handleButtonDisabled = () =>
+		textInput.length === 0 || isTextOnlyWhitespace(textInput);
+
+	return (
+		<>
+			<h1>Public Chat</h1>
+			<div className="chat-container">
+				<div className="message-frame">
+					<div>
+						{messages.map((message) => (
+							<div key={message.payload.id}>
+								<p>
+									<span>
+										{/* time stamp must be hour and minute only */}
+										{new Date(
+											message.payload.timestamp,
+										).toLocaleTimeString([], {
+											hour: "2-digit",
+											minute: "2-digit",
+											hour12: false,
+										})}
+										{" ~ "}
+									</span>
+									<b>{message.payload.username}</b>:{" "}
+									{message.payload.body}
+								</p>
+							</div>
+						))}
+						{isExternalTyping && (
+							<span className="is-typing">...</span>
+						)}
+					</div>
+				</div>
+
+				{username && (
+					<div className="input-container">
+						<input
+							type="text"
+							placeholder="Enter your message"
+							value={textInput}
+							onChange={handleInputChange}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									handleSendMessage();
+								}
+							}}
+						/>
+						<button
+							onClick={handleSendMessage}
+							disabled={handleButtonDisabled()}
+						>
+							Send
+						</button>
+					</div>
+				)}
+
+				{!username && (
+					<div className="input-container">
+						<input
+							type="text"
+							placeholder="Enter a username to start chatting"
+							value={usernameInput}
+							onChange={(e) => setUsernameInput(e.target.value)}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									if (usernameInput.length === 0) return;
+									if (isTextOnlyWhitespace(usernameInput))
+										return;
+									const username = usernameInput.trim();
+									setUsername(username);
+									setUsernameInput("");
+								}
+							}}
+						/>
+						<button
+							onClick={() => {
+								if (usernameInput.length === 0) return;
+								if (isTextOnlyWhitespace(usernameInput)) return;
+								const username = usernameInput.trim();
+								setUsername(username);
+								setUsernameInput("");
+							}}
+						>
+							Start
+						</button>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,52 +1,57 @@
-import { WebSocketServer } from 'ws';
-import * as dotenv from 'dotenv';
-import { authenticate } from './Authentication.js';
-import { mockMessages } from './mockedMessages.js';
+import { WebSocketServer } from "ws";
+import * as dotenv from "dotenv";
+import { authenticate } from "./Authentication.js";
+import { mockMessages } from "./mockedMessages.js";
 
 dotenv.config();
 
 type Success<T> = {
-  success: true;
-  value: T;
+	success: true;
+	value: T;
 };
 
 // A type to represent a failure outcome
 type Failure = {
-  success: false;
-  error: { message: string };
+	success: false;
+	error: { message: string };
 };
 
 // A union of the two, this is our Result type
 export type Result<T> = Success<T> | Failure;
 
 enum EVENTS {
-  CONNECTION = 'connection',
-  MESSAGE = 'message',
-  ERROR = 'error',
-  CLOSE = 'close',
+	CONNECTION = "connection",
+	MESSAGE = "message",
+	ERROR = "error",
+	CLOSE = "close",
 }
 
 export enum MESSAGE_TYPES {
-  ERROR = 'error',
-  TEXT = 'text',
-  TYPING_STATUS = 'typing_status',
-  ALL_MESSAGES = 'all_messages',
+	ERROR = "error",
+	TEXT = "text",
+	TYPING_STATUS = "typing_status",
+	ALL_MESSAGES = "all_messages",
 }
 
 export type AllMessages = {
-  type: MESSAGE_TYPES.ALL_MESSAGES;
-  payload: {
-    messages: Message[];
-  };
-}
+	type: MESSAGE_TYPES.ALL_MESSAGES;
+	payload: {
+		messages: Message[];
+	};
+};
 
 export type Message = {
-  type: MESSAGE_TYPES;
-  payload: {
-    body?: string;
-    isTyping?: boolean;
-  };
-}
+	type: MESSAGE_TYPES;
+	payload: {
+		id?: string; // Optional, for text messages
+		body?: string;
+		isTyping?: boolean;
+		username?: string;
+		timestamp?: string;
+	};
+};
+
+const memoryDabase: Message[] = mockMessages;
 
 const PORT = Number(process.env.PORT);
 const wsServer = new WebSocketServer({ port: PORT });
@@ -54,42 +59,67 @@ const wsServer = new WebSocketServer({ port: PORT });
 console.log(`Server is running on port ${PORT}`);
 
 wsServer.on(EVENTS.CONNECTION, (ws, req) => {
-  ws.send(JSON.stringify({ type: MESSAGE_TYPES.ALL_MESSAGES, payload: { messages: mockMessages } }));
-  // TODO: AUTH
-  // const authResult = authenticate(req);
-  // if (!authResult.success) {
-  //   console.error(`Authentication failed: ${authResult.error.message}`);
-  //   ws.send(JSON.stringify({
-  //     type: MESSAGE_TYPES.ERROR,
-  //     payload: { body: `Unauthorized: ${authResult.error.message}` }
-  //   }));
-  //   ws.terminate();
-  //   return; // Stop processing
-  // }
+	console.log("New client connected, sending current messages");
+	ws.send(
+		JSON.stringify({
+			type: MESSAGE_TYPES.ALL_MESSAGES,
+			payload: { messages: memoryDabase },
+		}),
+	);
 
-  ws.on(EVENTS.MESSAGE, (message) => {
-    try {
-      const data: Message = JSON.parse(message.toString());
-      console.log('Received message:', data);
+	ws.on(EVENTS.MESSAGE, (message) => {
+		try {
+			const data: Message = JSON.parse(message.toString());
+			console.log("Received message:", data);
 
-      if (data.type === MESSAGE_TYPES.TEXT) {
-        console.log('Received text message:', data.payload.body);
-      }
-      if (data.type === MESSAGE_TYPES.TYPING_STATUS) {
-        console.log('Received typing status:', data.payload.isTyping);
-      }
-
-    } catch (error) {
-      console.error('Error parsing message:', error);
-    }
-  });
+			if (data.type === MESSAGE_TYPES.TEXT) {
+				console.log("Received text message:", data.payload.body);
+				const newMessage: Message = {
+					type: MESSAGE_TYPES.TEXT,
+					payload: {
+						id: crypto.randomUUID(), // Generate a unique ID for the message
+						body: data.payload.body,
+						username: data.payload.username,
+						timestamp: new Date().toISOString(),
+					},
+				};
+				memoryDabase.push(newMessage);
+				wsServer.clients.forEach((client) => {
+					if (client.readyState === client.OPEN) {
+						client.send(
+							JSON.stringify({
+								type: MESSAGE_TYPES.TEXT,
+								payload: newMessage.payload,
+							}),
+						);
+					}
+				});
+			}
+			if (data.type === MESSAGE_TYPES.TYPING_STATUS) {
+				console.log("Received typing status:", data.payload.isTyping);
+				const typingStatus: Message = {
+					type: MESSAGE_TYPES.TYPING_STATUS,
+					payload: {
+						isTyping: data.payload.isTyping,
+						username: data.payload.username,
+					},
+				};
+				wsServer.clients.forEach((client) => {
+					if (client.readyState === client.OPEN) {
+						client.send(JSON.stringify(typingStatus));
+					}
+				});
+			}
+		} catch (error) {
+			console.error("Error parsing message:", error);
+		}
+	});
 });
 
 wsServer.on(EVENTS.ERROR, (error) => {
-  console.error('WebSocket error:', error);
+	console.error("WebSocket error:", error);
 });
 
 wsServer.on(EVENTS.CLOSE, () => {
-  console.log('WebSocket server closed');
+	console.log("WebSocket server closed");
 });
-
